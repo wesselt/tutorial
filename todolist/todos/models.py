1a8b9d264201fae042b4927ed22aa88f8c22b536
paulusdevries@gmail.com <paulusdevries@gmail.com>
2019-04-11 16:04:45 +0200

    Aanpassing in de relative import

--- todolist/todos/models.py
+++ todolist/todos/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import Permission, User
 from django.db import models
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 
 
 # Create your models here.
