fd05cf5d49452a99139c5adba31c54b7373e939f
Johan Winter <johan.winter@kpn.com>
2019-04-12 10:22:04 +0200

    col

--- todolist/todos/api_moodmon.py
+++ todolist/todos/api_moodmon.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
@@ -12,7 +12,7 @@ LOGGER = logging.getLogger(__name__)
 def signinout(request):
     result_respones = ""
     userdata = request.data
-
+    # login()
     if not userdata:
         return result_respones(
             "error",
